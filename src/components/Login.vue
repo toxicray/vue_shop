b43c4860bd5ee0c1ad47791e21682b0444f7b83f
ray <623888433@qq.com>
2020-05-23 18:18:36 +0800

    路由尝试

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        登录组件
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>   //scoped   样式只在组件下生效
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  routes: []
+  routes: [
+    {
+      path: '/login',
+      components: Login
+    }
+  ]
 })
