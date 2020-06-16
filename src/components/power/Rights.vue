cf402cf2320aa99fc23e9bcafac2b963b2e054f7
ray <623888433@qq.com>
2020-06-17 01:15:46 +0800

    yijiao

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">权限管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      123
+    </el-card>
+
+    <h3>权限组件界面</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rightsList: []
+    }
+  },
+  created() {
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('/rights/list')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限列表失败')
+      }
+      this.rightsList = res.data
+      console.log(this.rightsList)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
